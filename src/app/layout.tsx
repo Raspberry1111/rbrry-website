9b3da351b77484ec2edcd835036263c343312b2f
Raspberry1111 <raspberry1111@duck.com>
2024-08-16 12:52:12 -0400

    Add speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Topbar from "@/topbar";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,7 +21,9 @@ export default function RootLayout({
 			<body className={inter.className}>
 				<Topbar />
 				{children}
+				<SpeedInsights />
 			</body>
+
 		</html>
 	);
 }
