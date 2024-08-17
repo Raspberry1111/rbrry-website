a4068511acd95f42503abc8cd3b88c5511871676
Raspberry1111 <raspberry1111@duck.com>
2024-08-17 18:53:21 -0400

    Fix checkboxes and add sharp to optimize images

--- src/app/checkboxes/checkbox.tsx
+++ src/app/checkboxes/checkbox.tsx
@@ -1,34 +1,6 @@
-import { Dispatch, SetStateAction, useEffect, useState } from "react";
-
-function useCheckboxLocalStorage(id: number): [boolean, Dispatch<SetStateAction<boolean>>] {
-	const [value, setValue] = useState(false);
-	const [isFirst, setIsFirst] = useState(true);
-
-	const key = `checkbox-${id}`;
-
-	useEffect(() => {
-		const stored = localStorage.getItem(key);
-		setValue(stored ? JSON.parse(stored) : false);
-	}, [key]);
-
-	useEffect(() => {
-		setIsFirst(false);
-		if (isFirst) {
-			return;
-		}
-
-		if (!value) {
-			localStorage.removeItem(key);
-		} else {
-			localStorage.setItem(key, "true");
-		}
-	}, [key, value]);
-
-	return [value, setValue] as const;
-}
+import { useEffect, useState } from "react";
 
 export default function Checkbox({ checked, onClick }: { checked: boolean, onClick: (on: boolean) => void }) {
-	// const [isChecked, setIsChecked] = useCheckboxLocalStorage(id);
 	const [isChecked, setIsChecked] = useState(checked);
 
 	useEffect(() => {
