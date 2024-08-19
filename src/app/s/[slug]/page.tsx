c56e552eea876ab4121bc2e039255bb6455a3589
Raspberry1111 <raspberry1111@duck.com>
2024-08-19 11:06:18 -0400

    Add a link shortener

---
+++ src/app/s/[slug]/page.tsx
@@ -0,0 +1,40 @@
+"use server";
+
+import { createClient, Row } from "@libsql/client/web";
+import { notFound, permanentRedirect } from "next/navigation";
+
+// Next will cache fetch results by default, causing the database updates to not be received.
+function noCacheFetch(
+	input: string | URL | globalThis.Request,
+	init?: RequestInit,
+): Promise<Response> {
+	return fetch(input, { ...init, cache: 'no-store' });
+}
+
+async function getUrl(slug: string) {
+	"use server";
+
+	const turso = createClient({
+		url: process.env.TURSO_DATABASE_URL ?? "",
+		authToken: process.env.TURSO_AUTH_TOKEN,
+		fetch: noCacheFetch
+	});
+
+	const rows = (await turso.execute({ sql: "SELECT url FROM links WHERE id = ? LIMIT 1", args: [slug] })).rows;
+
+	console.log(rows);
+	const url = rows[0]?.url?.toString() ?? null;
+
+	if (url) {
+		console.log(url);
+		permanentRedirect(url);
+	} else {
+		notFound();
+	}
+}
+
+export default async function Shortener({ params }: { params: { slug: string } }) {
+	const slug = params.slug;
+
+	return <>{await getUrl(slug)}</>;
+}
